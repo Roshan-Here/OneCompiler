f96b53c542989ad9b4ce091ba7940add8ab7a056
Muh Roshan <leoparmigiana@gmail.com>
2024-03-24 01:19:35 +0530

    basic settup for react frontend done

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <p className='text-xl text-violet-800 border-b-indigo-400'>Welcome to OneCompiler</p>
+    </>
+  )
+}
+
+export default App
