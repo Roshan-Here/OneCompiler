a109ed76440856b3f0e9a1cea034b4bdb0fe12ae
Muh Roshan <leoparmigiana@gmail.com>
2024-03-30 04:09:17 +0530

    theme selector added - need to setup updatetheme onclick

--- client/src/pages/Compile.jsx
+++ client/src/pages/Compile.jsx
@@ -1,54 +1,81 @@
 import Editor, {useMonaco } from '@monaco-editor/react'
 import React, { useEffect, useState } from 'react'
-import GitHubDarkTheme from "monaco-themes/themes/GitHub Dark.json";
-import TomorrowNightTheme from "monaco-themes/themes/Tomorrow-Night.json";
-import CobaltTheme from "monaco-themes/themes/Cobalt.json";
-import OceanicNextTheme from "monaco-themes/themes/Oceanic Next.json";
 import monokai from "monaco-themes/themes/Monokai.json";
+import nightOwl from "monaco-themes/themes/Night Owl.json";
 import themeMap from '../utils/themelist';
 
 function Compile() {
     const deftmsg = `Welcome to Onecompiler where anything can be compiled`
     const monaco = useMonaco()
+    const [theme,setTheme] = useState("monokai")
+    const [language,setlanguage] = useState("python")
     const [code,setcode] = useState("")
-    const [theme,setTheme] = useState(themeMap.Active4D)
     const handlechange=(value)=>{
         setcode(value)
     }
+    console.log(code)
     const handleTheme=(value)=>{
+        monaco.editor.defineTheme("wow",value)
+        monaco.editor.setTheme("wow");
+        console.log(value)
         setTheme(value)
     }
-    console.log(code)
+
     useEffect(() => {
         if (monaco) {
-          monaco.editor.defineTheme("monokai", monokai);
-          monaco.editor.setTheme("monokai");
+          monaco.editor.defineTheme("newtheme", monokai);
+          monaco.editor.setTheme("newtheme");
         }
       }, [monaco]);
+
+      const ThemeList = ({ themeMap }) => {
+        const themeName = Object.keys(themeMap);
+        return (
+          <ul>
+            {themeName.map((themName, index) => {
+              return (
+                <li key={index} onClick={()=>handleTheme(themName)}>
+                  <a>{themName}</a>
+                </li>
+              );
+            })}
+          </ul>
+        );
+      };
+
     return (
     <div className='bg-gray-900 min-h-lvh'>
         {/* theme selector,language selector,run button */}
         <div className='flex flex-row justify-between'>
             <div className='flex justify-center px-6 mt-3'>
-                <div className='px-6 '>Language seletor</div>
-                <div className='px-12 '>Theme seletor</div>
+                <div className='px-6'>
+                <div className="dropdown dropdown-hover">
+                    <option className='font-semibold text-slate-300'>Theme Selector</option>
+                    <div tabIndex={0} role="button" className="btn btn-block btn-neutral btn-outline btn-primary mt-1 ">{theme}</div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <ThemeList themeMap={themeMap}/>
+                        {/* <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li> */}
+                    </ul>
+                    </div>
+                </div>
+                <div className='px-12 '>Select Language</div>
             </div>
-            {/* <div className='px-6 mt-3'>Theme seletor</div> */}
             <div className='px-6 mt-3'>Run button</div>
         </div>
       <div className='flex flex-col justify-between md:flex-row'>
-        <div className='md:w-3/5 w-full p-6 h-screen'>
+        <div className='md:w-3/5 w-full px-6 mt-3 h-screen'>
             <Editor
             // error ! - border-red-500
             className='border rounded-lg border-cyan-500'
-            theme="vs-black"
-            defaultLanguage="vs-code"
+            theme={theme}
+            defaultLanguage={language}
             defaultValue={deftmsg}
             onChange={handlechange}
             ></Editor>
         </div>
         
-        <div className='md:w-2/5 p-6 w-full'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit placeat exercitationem laboriosam ut quaerat vel ab unde, temporibus sed cum? Asperiores rerum earum assumenda, atque accusantium eveniet. Fuga, accusamus? Tempora!</div>
+        <div className='md:w-2/5 px-6 mt-3 w-full'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit placeat exercitationem laboriosam ut quaerat vel ab unde, temporibus sed cum? Asperiores rerum earum assumenda, atque accusantium eveniet. Fuga, accusamus? Tempora!</div>
       </div>
     </div>
   )
--- client/src/utils/themelist.js
+++ client/src/utils/themelist.js
@@ -1,112 +1,54 @@
-// copied from --
-// https://github.com/rohit5591/chrome-extension-crx-power-editor/blob/fd6f37e510085b8328a58f3d48095ab470ea135b/src/core/power-editor/monaco-editor.js#L34
-import Active4D from "monaco-themes/themes/Active4D.json";
-import AllHallowsEve from "monaco-themes/themes/All Hallows Eve.json";
-import Amy from "monaco-themes/themes/Amy.json";
-import BirdsofParadise from "monaco-themes/themes/Birds of Paradise.json";
-import Blackboard from "monaco-themes/themes/Blackboard.json";
-import BrillianceBlack from "monaco-themes/themes/Brilliance Black.json";
-import BrillianceDull from "monaco-themes/themes/Brilliance Dull.json";
-import ChromeDevTools from "monaco-themes/themes/Chrome DevTools.json";
-import CloudsMidnight from "monaco-themes/themes/Clouds Midnight.json";
-import Clouds from "monaco-themes/themes/Clouds.json";
-import Cobalt from "monaco-themes/themes/Cobalt.json";
-import Cobalt2 from "monaco-themes/themes/Cobalt2.json";
-import Dawn from "monaco-themes/themes/Dawn.json";
-import DominionDay from "monaco-themes/themes/Dominion Day.json";
-import Dracula from "monaco-themes/themes/Dracula.json";
-import Dreamweaver from "monaco-themes/themes/Dreamweaver.json";
-import Eiffel from "monaco-themes/themes/Eiffel.json";
-import EspressoLibre from "monaco-themes/themes/Espresso Libre.json";
-import GitHub from "monaco-themes/themes/GitHub.json";
-import IDLE from "monaco-themes/themes/IDLE.json";
-import idleFingers from "monaco-themes/themes/idleFingers.json";
-import iPlastic from "monaco-themes/themes/iPlastic.json";
-import Katzenmilch from "monaco-themes/themes/Katzenmilch.json";
-import krTheme from "monaco-themes/themes/krTheme.json";
-import KuroirTheme from "monaco-themes/themes/Kuroir Theme.json";
-import LAZY from "monaco-themes/themes/LAZY.json";
-import MagicWBAmiga from "monaco-themes/themes/MagicWB (Amiga).json";
-import MerbivoreSoft from "monaco-themes/themes/Merbivore Soft.json";
-import Merbivore from "monaco-themes/themes/Merbivore.json";
-import monoindustrial from "monaco-themes/themes/monoindustrial.json";
-import MonokaiBright from "monaco-themes/themes/Monokai Bright.json";
-import Monokai from "monaco-themes/themes/Monokai.json";
-import NightOwl from "monaco-themes/themes/Night Owl.json";
-import Nord from "monaco-themes/themes/Nord.json";
-import OceanicNext from "monaco-themes/themes/Oceanic Next.json";
-import PastelsonDark from "monaco-themes/themes/Pastels on Dark.json";
-import SlushandPoppies from "monaco-themes/themes/Slush and Poppies.json";
-import Solarizeddark from "monaco-themes/themes/Solarized-dark.json";
-import Solarizedlight from "monaco-themes/themes/Solarized-light.json";
-import SpaceCadet from "monaco-themes/themes/SpaceCadet.json";
-import Sunburst from "monaco-themes/themes/Sunburst.json";
-import TextmateMacClassic from "monaco-themes/themes/Textmate (Mac Classic).json";
-import Tomorrow from "monaco-themes/themes/Tomorrow.json";
-import TomorrowNight from "monaco-themes/themes/Tomorrow-Night.json";
-import TomorrowNightBlue from "monaco-themes/themes/Tomorrow-Night-Blue.json";
-import TomorrowNightBright from "monaco-themes/themes/Tomorrow-Night-Bright.json";
-import TomorrowNightEighties from "monaco-themes/themes/Tomorrow-Night-Eighties.json";
-import Twilight from "monaco-themes/themes/Twilight.json";
-import UpstreamSunburst from "monaco-themes/themes/Upstream Sunburst.json";
-import VibrantInk from "monaco-themes/themes/Vibrant Ink.json";
-import Xcode_default from "monaco-themes/themes/Xcode_default.json";
-import Zenburnesque from "monaco-themes/themes/Zenburnesque.json";
 
 const themeMap = {
-    'Active4D': Active4D,
-    'Xcode_default': Xcode_default,
-    'AllHallowsEve': AllHallowsEve,
-    'Amy': Amy,
-    'BirdsofParadise': BirdsofParadise,
-    'Blackboard': Blackboard,
-    'BrillianceBlack': BrillianceBlack,
-    'BrillianceDull': BrillianceDull,
-    'ChromeDevTools': ChromeDevTools,
-    'CloudsMidnight': CloudsMidnight,
-    'Clouds': Clouds,
-    'Cobalt': Cobalt,
-    'Cobalt2': Cobalt2,
-    'Dawn': Dawn,
-    'DominionDay': DominionDay,
-    'Dracula': Dracula,
-    'Dreamweaver': Dreamweaver,
-    'Eiffel': Eiffel,
-    'EspressoLibre': EspressoLibre,
-    'GitHub': GitHub,
-    'IDLE': IDLE,
-    'idleFingers': idleFingers,
-    'iPlastic': iPlastic,
-    'Katzenmilch': Katzenmilch,
-    'krTheme': krTheme,
-    'KuroirTheme': KuroirTheme,
-    'LAZY': LAZY,
-    'MagicWBAmiga': MagicWBAmiga,
-    'MerbivoreSoft': MerbivoreSoft,
-    'Merbivore': Merbivore,
-    'monoindustrial': monoindustrial,
-    'MonokaiBright': MonokaiBright,
-    'Monokai': Monokai,
-    'NightOwl': NightOwl,
-    'Nord': Nord,
-    'OceanicNext': OceanicNext,
-    'PastelsonDark': PastelsonDark,
-    'SlushandPoppies': SlushandPoppies,
-    'Solarizeddark': Solarizeddark,
-    'Solarizedlight': Solarizedlight,
-    'SpaceCadet': SpaceCadet,
-    'Sunburst': Sunburst,
-    'TextmateMacClassic': TextmateMacClassic,
-    'TomorrowNightBlue': TomorrowNightBlue,
-    'TomorrowNightBright': TomorrowNightBright,
-    'TomorrowNightEighties': TomorrowNightEighties,
-    'TomorrowNight': TomorrowNight,
-    'Tomorrow': Tomorrow,
-    'Twilight': Twilight,
-    'UpstreamSunburst': UpstreamSunburst,
-    'VibrantInk': VibrantInk,
-    'Xcodedefault': Xcode_default,
-    'Zenburnesque': Zenburnesque
+    active4d: "Active4D",
+    "all-hallows-eve": "All Hallows Eve",
+    amy: "Amy",
+    "birds-of-paradise": "Birds of Paradise",
+    blackboard: "Blackboard",
+    "brilliance-black": "Brilliance Black",
+    "brilliance-dull": "Brilliance Dull",
+    "chrome-devtools": "Chrome DevTools",
+    "clouds-midnight": "Clouds Midnight",
+    clouds: "Clouds",
+    cobalt: "Cobalt",
+    dawn: "Dawn",
+    dreamweaver: "Dreamweaver",
+    eiffel: "Eiffel",
+    "espresso-libre": "Espresso Libre",
+    github: "GitHub",
+    idle: "IDLE",
+    katzenmilch: "Katzenmilch",
+    "kuroir-theme": "Kuroir Theme",
+    lazy: "LAZY",
+    "magicwb--amiga-": "MagicWB (Amiga)",
+    "merbivore-soft": "Merbivore Soft",
+    merbivore: "Merbivore",
+    "monokai-bright": "Monokai Bright",
+    monokai: "Monokai",
+    "nightOwl": "Night Owl",
+    "oceanic-next": "Oceanic Next",
+    "pastels-on-dark": "Pastels on Dark",
+    "slush-and-poppies": "Slush and Poppies",
+    "solarized-dark": "Solarized-dark",
+    "solarized-light": "Solarized-light",
+    spacecadet: "SpaceCadet",
+    sunburst: "Sunburst",
+    "textmate--mac-classic-": "Textmate (Mac Classic)",
+    "tomorrow-night-blue": "Tomorrow-Night-Blue",
+    "tomorrow-night-bright": "Tomorrow-Night-Bright",
+    "tomorrow-night-eighties": "Tomorrow-Night-Eighties",
+    "tomorrow-night": "Tomorrow-Night",
+    tomorrow: "Tomorrow",
+    twilight: "Twilight",
+    "upstream-sunburst": "Upstream Sunburst",
+    "vibrant-ink": "Vibrant Ink",
+    "xcode-default": "Xcode_default",
+    zenburnesque: "Zenburnesque",
+    iplastic: "iPlastic",
+    idlefingers: "idleFingers",
+    krtheme: "krTheme",
+    monoindustrial: "monoindustrial",
 };
+
 
 export default themeMap;
