664720cb555896f3c18fe4bbdbb4dd6dc55eaca4
Muh Roshan <leoparmigiana@gmail.com>
2024-03-30 20:28:37 +0530

    Theme selection Done!, added some badic themes - may add more later--> nxt language selection

--- client/src/pages/Compile.jsx
+++ client/src/pages/Compile.jsx
@@ -1,50 +1,55 @@
 import Editor, {useMonaco } from '@monaco-editor/react'
 import React, { useEffect, useState } from 'react'
-import monokai from "monaco-themes/themes/Monokai.json";
-import nightOwl from "monaco-themes/themes/Night Owl.json";
+import dracula from "monaco-themes/themes/Dracula.json";
 import themeMap from '../utils/themelist';
 import Footer from './../components/Footer';
 
 function Compile() {
     const deftmsg = `Welcome to Onecompiler where anything can be compiled`
     const monaco = useMonaco()
-    const [theme,setTheme] = useState("monokai")
+    const [themename,setThemename] = useState("Dracula")
+    const [theme,setTheme] = useState()
     const [language,setlanguage] = useState("python")
     const [code,setcode] = useState("")
     const handlechange=(value)=>{
         setcode(value)
     }
     console.log(code)
-    const handleTheme=(value)=>{
+    const handleTheme=(value,name)=>{
         setTheme(value)
-        // monaco.editor.updateOptions({ theme: theme});
+        setThemename(name)
+        if (monaco){
+            monaco.editor.defineTheme("updatetheme", value);
+            monaco.editor.setTheme("updatetheme");
+        }
         console.log(value)
     }
 
     useEffect(() => {
         if (monaco) {
-          monaco.editor.defineTheme("newtheme", monokai);
+          monaco.editor.defineTheme("newtheme", dracula);
           monaco.editor.setTheme("newtheme");
         }
       }, [monaco]);
 
-      const ThemeList = ({ themeMap }) => {
-        const themeName = Object.keys(themeMap);
+      const ThemeList = () => {
         return (
-          <ul className='collapse'>
-            {themeName.map((themName, index) => {
-              return (
-                <li key={index} className='collapse-open' onClick={()=>handleTheme(themName)}>
-                  <a>{themName}</a>
-                </li>
+        <>
+            {themeMap.map((themee) => {
+                return (
+                <ul className='collapse' key={themee.id}>
+                    <li key={themee.id} className='collapse-open' onClick={()=>handleTheme(themee.changeto,themee.name)}>
+                        <a>{themee.name}</a>
+                    </li>
+                </ul>
               );
             })}
-          </ul>
+          </>
         );
       };
 
     return (
-    <compiler>
+    <section>
     <div className=' bg-gray-900 bg-auto'>
         {/* theme selector,language selector,run button */}
         <div className='flex flex-row justify-between'>
@@ -52,9 +57,9 @@ function Compile() {
                 <div className='px-6'>
                 <div className="dropdown dropdown-hover">
                     <option className='font-semibold text-slate-300'>Theme Selector</option>
-                    <div tabIndex={0} role="button" className="btn btn-block btn-neutral btn-outline btn-primary mt-1">{theme}</div>
-                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                            <ThemeList themeMap={themeMap}/>
+                    <div tabIndex={0} role="button" className="btn btn-block btn-neutral btn-outline btn-primary mt-1">{themename}</div>
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-slate-800 rounded-box w-52">
+                            <ThemeList/>
                         </ul>
                     </div>
                 </div>
@@ -78,7 +83,7 @@ function Compile() {
       </div>
     </div>
     <Footer/>
-    </compiler>
+    </section>
   )
 }
 
--- client/src/utils/themelist.js
+++ client/src/utils/themelist.js
@@ -1,17 +1,40 @@
+import nightOwl from "monaco-themes/themes/Night Owl.json";
+import monokai from "monaco-themes/themes/Monokai.json";
+import githubdark from "monaco-themes/themes/GitHub Dark.json";
+import githublight from "monaco-themes/themes/GitHub Light.json";
+import dracula from "monaco-themes/themes/Dracula.json";
 
 
-const themeMap = {
-    active4d: "Active4D",
-    "all-hallows-eve": "All Hallows Eve",
-    "birds-of-paradise": "Birds of Paradise",
-    blackboard: "Blackboard",
-    "brilliance-black": "Brilliance Black",
-    "chrome-devtools": "Chrome DevTools",
-    "clouds-midnight": "Clouds Midnight",
-    clouds: "Clouds",
-    cobalt: "Cobalt",
-    monokai: "Monokai",
-    "night-owl": "Night Owl",
-};
+const themeMap = [
+    {
+        id: 1,
+        name: "nightOwl",
+        changeto: nightOwl,
+    },
+    {
+        id: 2,
+        name: "monokai",
+        changeto: monokai,
+    },
+    {
+        id: 3,
+        name: "Github-dark",
+        changeto: githubdark,
+    },
+    {
+        id: 4,
+        name: "Github-light",
+        changeto: githublight,
+    },
+    {
+        id: 5,
+        name: "Dracula",
+        changeto: dracula,
+    },
+];
+
+// "github-dark": "GitHub Dark",
+//     "github-light": "GitHub Light",
+// "dracula": "Dracula",
 
 export default themeMap;
