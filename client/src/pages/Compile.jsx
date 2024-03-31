79198db36a9bc93c5aafc0b9b6a13bd3687d8596
Muh Roshan <leoparmigiana@gmail.com>
2024-03-31 14:47:00 +0530

    compile page, top-button ui mostly completed with live updating Editor, nxt-> need to handle some buttons and need to complete right side.

--- client/src/pages/Compile.jsx
+++ client/src/pages/Compile.jsx
@@ -3,13 +3,17 @@ import React, { useEffect, useState } from 'react'
 import dracula from "monaco-themes/themes/Dracula.json";
 import themeMap from '../utils/themelist';
 import Footer from './../components/Footer';
+import languagesList from '../utils/listLanguges';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCirclePlay, faFloppyDisk} from '@fortawesome/free-solid-svg-icons'
 
 function Compile() {
-    const deftmsg = `Welcome to Onecompiler where anything can be compiled`
     const monaco = useMonaco()
-    const [themename,setThemename] = useState("Dracula")
+    const [deftmsg,setdeftmsg] = useState("Welcome to Onecompiler where anything can be compiled")
+    const [themename,setThemename] = useState("Theme")
     const [theme,setTheme] = useState()
-    const [language,setlanguage] = useState("python")
+    const [language,setlanguage] = useState("Languages")
+    const [langforbutton, setlangforbutton] = useState("Languages")
     const [code,setcode] = useState("")
     const handlechange=(value)=>{
         setcode(value)
@@ -33,20 +37,44 @@ function Compile() {
       }, [monaco]);
 
       const ThemeList = () => {
-        return (
-        <>
-            {themeMap.map((themee) => {
-                return (
-                <ul className='collapse' key={themee.id}>
-                    <li key={themee.id} className='collapse-open' onClick={()=>handleTheme(themee.changeto,themee.name)}>
-                        <a>{themee.name}</a>
-                    </li>
-                </ul>
+      return (
+      <>
+          {themeMap.map((themee) => {
+              return (
+              <ul className='collapse' key={themee.id}>
+                  <li key={themee.id} className='collapse-open hover:cursor-pointer' onClick={()=>handleTheme(themee.changeto,themee.name)}>
+                      <a>{themee.name}</a>
+                  </li>
+              </ul>
               );
-            })}
+          })}
           </>
-        );
+      );
       };
+
+    const handlelanguage = (value) =>{
+        setlanguage(value.name)
+        setlangforbutton(value.value)
+        setdeftmsg(value.initCode)
+    }
+    console.log(deftmsg)
+
+    const LangList = () => {
+    return (
+        <>
+        {languagesList.map((lang) => {
+            return (
+            <ul className='collapse menu' key={lang.id}>
+                <li key={lang.id} className='collapse-open hover:cursor-pointer' onClick={()=>handlelanguage(lang)}>
+                    <a>{lang.name}</a>
+                </li>
+            </ul>
+            );
+        })}
+        </>
+    );
+    };
+
 
     return (
     <section>
@@ -56,25 +84,45 @@ function Compile() {
             <div className='flex justify-center px-6 mt-3'>
                 <div className='px-6'>
                 <div className="dropdown dropdown-hover">
-                    <option className='font-semibold text-slate-300'>Theme Selector</option>
+                    {/* <option className='font-semibold text-slate-300'>Theme Selector</option> */}
                     <div tabIndex={0} role="button" className="btn btn-block btn-neutral btn-outline btn-primary mt-1">{themename}</div>
                         <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-slate-800 rounded-box w-52">
                             <ThemeList/>
                         </ul>
                     </div>
                 </div>
-                <div className='px-12 '>Select Language</div>
+                <div className='px-5'>
+                <div className="dropdown dropdown-hover">
+                    {/* <option className='font-semibold text-slate-300'>Select Language</option> */}
+                    <div tabIndex={0} role="button" className="btn btn-block btn-neutral btn-outline btn-accent mt-1">{language}</div>
+                        <ul tabIndex={0} className="dropdown-content z-[1] shadow bg-slate-800 rounded-box w-56 max-h-48 overflow-y-auto">
+                            <LangList/>
+                        </ul>
+                    </div>
+                </div>
+                <div className='px-5'>
+                    <div role='button' className='hidden md:btn btn-block btn-neutral border-r-2 border-green-500 hover:border-cyan-500 mt-1 hover:text-green-400 font-semibold text-white'>
+                        Save
+                    <FontAwesomeIcon className='text-2xl text-blue-400 transition-colors' icon={faFloppyDisk} />
+                    </div>
+                </div>
             </div>
-            <div className='px-6 mt-3'>Run button</div>
+            <div className='px-6 mt-3'>
+                <div role='button' className='btn btn-block btn-neutral border-r-2 border-green-500 hover:border-cyan-500 mt-1 hover:text-green-400 font-semibold text-white'>
+                <FontAwesomeIcon className='text-2xl text-green-500 transition-colors' icon={faCirclePlay} />
+                    Run
+                </div>
+                {/* <button className='bg-success p-3 border border-green-400 hover:border-blue-600 rounded-md font-semibold text-white'>Run</button> */}
+            </div>
         </div>
       <div className='flex flex-col justify-between md:flex-row'>
-        <div className='md:w-3/5 w-full px-6 mt-3 h-screen'>
+        <div className='md:w-3/5 w-full px-6 m-3 h-screen'>
             <Editor
-            // error ! - border-red-500
+            // error ! - border-red-500 : border-cyan-500
             className='border rounded-lg border-cyan-500'
-            theme={theme}
-            defaultLanguage={language}
-            defaultValue={deftmsg}
+            theme={theme || "Dracula"}
+            language={langforbutton || "python"}
+            value={deftmsg}
             onChange={handlechange}
             ></Editor>
         </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,27 +6,27 @@ import Footer from '../components/Footer'
 function Home() {
   return (
     <div className='bg-gray-900 min-h-lvh'>
-        <div className='flex md:justify-center p-5'>
+        <div className='flex md:justify-center p-5 hover:cursor-default'>
             <div className='text-5xl md:text-7xl text-slate-200 font-extrabold'>Accelerate your career</div>
             {/* <div className='text-2xl text-slate-400 font-bold'>Live learning with industry experts. Jobs at technology companies.</div> */}
         </div>
-        <div className='flex px-5 md:justify-center md:p-1'>
+        <div className='flex px-5 md:justify-center md:p-1 hover:cursor-default'>
             {/* <div className='text-5xl md:text-7xl text-slate-200 font-extrabold'>Accelerate your career</div> */}
             <div className='text-2xl text-slate-400 font-bold'>OneCompiler is the best platform to help you enhance your skills, expand your knowledge,</div>
         </div>
         {/* new box*/}
-        <div className='m-8 flex flex-col rounded-2xl shadow-lg p-2 mt-10 md:ml-6 md:mr-6 md:flex-row bg-white'>
+        <div className='m-8 flex flex-col rounded-2xl shadow-lg p-2 mt-10 md:ml-6 md:mr-6 md:flex-row bg-white hover:cursor-default'>
           <img src="https://picsum.photos/800/400" alt=""
             className='h-60 md:w-80 rounded-xl bg-center bg-cover bg-gray-100'
           ></img>
           <div className='flex flex-col gap-1 p-2 md:p-6'>
             <div className='text-gray-700 font-bold text-4xl'>Developer</div>
-            <p className='text-gray-500'>
+            <p className='text-gray-800'>
             We now support 
             <span className='font-bold text-gray-800'> 80 </span>
              popular coding languages. At our core, 
              <span className='font-bold text-blue-600'> One</span>
-             <span className='font-semibold text-cyan-300'>Compiler </span>
+             <span className='font-semibold text-cyan-400'>Compiler </span>
               is about developers. Our powerful development tools such as Playground help you test, debug and even write your own projects online
             </p>
             <div className='flex gap-4 mt-auto ml-auto'>
@@ -41,10 +41,10 @@ function Home() {
         </div>
 
         {/* sample languages... 6,5,4 -format*/}
-        <div className='flex text-4xl font-bold justify-center text-slate-100'>
+        <div className='flex text-4xl font-bold justify-center text-slate-100 hover:cursor-default'>
             Available languages
         </div>
-        <div className='m-8 border rounded-2xl gap-5 shadow-lg bg-cyan-950'>
+        <div className='m-8 border rounded-2xl gap-5 shadow-lg bg-cyan-950 hover:cursor-default'>
             {/* part one of lang 6 */}
             <div className='flex flex-row justify-between md:flex-row mt-2 font-bold ml-7 mr-7'>
                   <div className="flex flex-col p-3 text-center w-1/3 md:w-1/12 text-slate-100">
