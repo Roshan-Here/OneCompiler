100286c38340e82a94ca774b15e6fd5c29971791
Muh Roshan <leoparmigiana@gmail.com>
2024-06-11 01:33:08 +0530

    ui updated for problems page

--- client/src/pages/Problems.jsx
+++ client/src/pages/Problems.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faShuffle } from "@fortawesome/free-solid-svg-icons";
 
 function Problems() {
     const difficultyList = ["Easy","Medium","Hard"]
@@ -13,23 +15,24 @@ function Problems() {
       <div className="flex justify-center text-4xl font-bold text-gray-200 overflow-hidden">
         <div className="p-5 underline">Problem List</div>
       </div>
-      <div className="flex justify-around">
-        <div className="p-3">sort by</div>
+      <div className="flex justify-around gap-2">
+        <div className="hidden md:flex p-3 text-xl font-medium">SortBy:</div>
         {/* Difficulty button */}
-        <div className="dropdown dropdown-bottom dropdown-end relative">
+        <div className="dropdown dropdown-hover relative">
             <div tabIndex={0} role="button" className="btn btn-block btn-neutral btn-outline btn-accent mt-1">Difficulty</div>
-            <ul tabIndex={0} className="dropdown-content px-8 absolute z-50 shadow bg-slate-800 rounded-box w-44 max-h-48 overflow-y-auto collapse-open hover:cursor-pointer">
+            <ul tabIndex={0} className="dropdown-content px-3 absolute z-50 shadow bg-slate-800 rounded-box w-44  overflow-y-auto collapse-open hover:cursor-pointer">
                 {
                     difficultyList.map((diff)=>
                         (
+                            <ul className='collapse menu' key={diff}>
                             <li 
-                                key={diff}
                                 className={diff==="Easy"? 
                                 "text-green-400"
                                 : diff === "Medium"?
                                 "text-yellow-300"
                                 :"text-red-600"
-                            } onClick={()=>handleDifficulty(diff)}><a>{diff}</a></li>                        
+                            } onClick={()=>handleDifficulty(diff)}><a>{diff}</a></li>
+                            </ul>                        
                         )
                     )
                 }
@@ -38,14 +41,36 @@ function Problems() {
             <li className="text-red-600"><a>Hard</a></li> */}
         </ul>
         </div>
-        <div className="p-3">Tags</div>
-        <div className="p-3">Search Bar</div>
-        <div className="p-3">Random</div>
+        <div className="">
+            <div className="px-6 md:px-9 btn btn-block btn-info btn-outline btn-accent mt-1">
+                Tags
+            </div>
+        </div>
+        <div className="">
+            {/* <input type="text" placeholder="Search Problems" className="px-4 md:px-12 input input-bordered input-success w-full max-w-xs mt-1" /> */}
+            <label className=" input input-bordered input-success flex items-center gap-2 mt-1">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg> */}
+            <input type="text" className="grow" placeholder="Search Problems" />
+            </label>
+
+        </div>
+        <div className="">
+            <div className="p-3 rounded-3xl bg-green-600 dropdown dropdown-left dropdown-bottom  dropdown-hover">
+            <FontAwesomeIcon className="text-2xl text-black" icon={faShuffle} />
+            <div tabIndex={0} className="card compact dropdown-content z-[1] shadow bg-base-100 rounded-box w-48">
+                <div tabIndex={0} className="card-body bg-gray-900 text-green-500">
+                {/* <h2 className="card-title">You needed more info?</h2>  */}
+                <p>Random Question selector!</p>
+                </div>
+            </div>
+            </div>
+        </div>
       </div>
       <div className="md:px-40 flex justify-center overflow-x-auto">
         <table className="table">
           {/* head */}
-          <thead>
+          <thead className="text-xl font-normal">
             <tr>
               <th>No</th>
               <th>Title</th>
