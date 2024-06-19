18bf506160455437831dc92773655881a69aa00b
Muh Roshan <leoparmigiana@gmail.com>
2024-06-19 14:39:36 +0530

    static files added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -32,3 +32,5 @@ urlpatterns = [
     # path("api/token/", TokenObtainPairView.as_view(), name="get-token"),
     path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
